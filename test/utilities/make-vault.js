6835d9970d555846d46e0c8f30384258cfe7dc8d
JasperSlimmens <jasper@slimmens.nl>
2021-02-19 14:35:19 +0100

    Change to Hardhat environment.

--- test/utilities/make-vault.js
+++ test/utilities/make-vault.js
@@ -1,10 +1,9 @@
-const Vault = artifacts.require("Vault");
+const Vault = artifacts.require("IVault");
 const VaultProxy = artifacts.require("VaultProxy");
 
-module.exports = async function(...args) {
+module.exports = async function(implementationAddress, ...args) {
   const fromParameter = args[args.length - 1]; // corresponds to {from: governance}
-  const vaultImplementation = await Vault.new(fromParameter);
-  const vaultAsProxy = await VaultProxy.new(vaultImplementation.address, fromParameter);
+  const vaultAsProxy = await VaultProxy.new(implementationAddress, fromParameter);
   const vault = await Vault.at(vaultAsProxy.address);
   await vault.initializeVault(...args);
   return vault;
