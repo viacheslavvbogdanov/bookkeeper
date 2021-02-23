972bff2d0d6974dd962a65a326a018abcd399293
re76 <rernst76@gmail.com>
2021-02-22 21:59:09 -0500

    Adding more tests for StringUtil lib

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-truffle5");
 require("hardhat-gas-reporter");
 
-const keys = require('./dev-keys.json');
+const keys = require("./dev-keys.json");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -14,37 +14,38 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: true,
       forking: {
-        //url: "https://mainnet.infura.io/v3/" + keys.infuraKey,
         url: "https://eth-mainnet.alchemyapi.io/v2/" + keys.alchemyKeyMainnet,
-        blockNumber: 11900760
-      }
+        blockNumber: 11900760,
+      },
     },
     ropsten: {
       allowUnlimitedContractSize: true,
       url: "https://eth-ropsten.alchemyapi.io/v2/" + keys.alchemyKeyRopsten,
-      accounts: ["0x" + keys.ropstenPrivateKey]
-    }
+      accounts: ["0x" + keys.ropstenPrivateKey],
+    },
   },
   etherscan: {
-    apiKey: keys.etherscanAPI
+    apiKey: keys.etherscanAPI,
   },
   solidity: {
     compilers: [
-      {version: "0.6.12",
-       settings: {
-         optimizer: {
-           enabled: true,
-           runs: 150
-         }
-       }},
-    ]
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 150,
+          },
+        },
+      },
+    ],
   },
   mocha: {
-    timeout: 2000000
+    timeout: 2000000,
   },
   gasReporter: {
     enabled: true,
-    currency: 'USD',
+    currency: "USD",
     gasPrice: 100,
-  }
+  },
 };
--- test/test-StringUtil.js
+++ test/test-StringUtil.js
@@ -15,5 +15,45 @@ describe("StringUtil", function () {
     it("should return one for single char strings", async function () {
       expect(await stringUtil.strLength("a")).to.equal(1);
     });
+    it("should return many for many char strings", async function () {
+      expect(await stringUtil.strLength("abc")).to.equal(3);
+    });
+  });
+  describe("strConcat", async function () {
+    it("should handle two empty strings", async function () {
+      expect(await stringUtil.strConcat("", "")).to.equal("");
+    });
+    it("should handle empty 'a' atring", async function () {
+      expect(await stringUtil.strConcat("", "foo")).to.equal("foo");
+    });
+    it("should handle empty 'b' string", async function () {
+      expect(await stringUtil.strConcat("bar", "")).to.equal("bar");
+    });
+    it("should concat two characters", async function () {
+      expect(await stringUtil.strConcat("a", "b")).to.equal("ab");
+    });
+    it("should concat longer strigns characters", async function () {
+      expect(await stringUtil.strConcat("foo", "bar")).to.equal("foobar");
+    });
+  });
+  describe("strCompare", async function () {
+    it("should return true for two empty strings", async function () {
+      expect(await stringUtil.strCompare("", "")).to.be.true;
+    });
+    it("should return false for one empty string and one char string", async function () {
+      expect(await stringUtil.strCompare("a", "")).to.be.false;
+    });
+    it("should return true for two equal single char strings", async function () {
+      expect(await stringUtil.strCompare("a", "a")).to.be.true;
+    });
+    it("should return false for two different single char strings", async function () {
+      expect(await stringUtil.strCompare("a", "b")).to.be.false;
+    });
+    it("should return false for two different strings", async function () {
+      expect(await stringUtil.strCompare("foo", "bar")).to.be.false;
+    });
+    it("should return true for two equal strings", async function () {
+      expect(await stringUtil.strCompare("foo", "foo")).to.be.true;
+    });
   });
 });
