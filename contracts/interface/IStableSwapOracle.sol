fa6cd3e56a1e9bc3bde85e82a4e6187df02bd035
thegismar <thegismar@gmail.com>
2021-05-22 15:50:15 -0300

    Delete IStableSwapOracle.sol

--- contracts/interface/IStableSwapOracle.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.12;
-
-interface IStableSwapOracle {
-    function getRate(string memory _name, address _token0, address _token1) external view returns (bool, uint256);
-}
